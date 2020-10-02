5b2a18d89774428947340d215c60e7f788ebd7dd
Jiho Ro <jihoandy@gmail.com>
2020-10-01 20:08:56 -0500

    small changes to original

---
+++ scripts/train.sh
@@ -0,0 +1,21 @@
+#!/bin/bash 
+
+# Author: Wei-Ning Hsu
+
+
+vqon=$1    # 01000
+name=$2    # RDVQ_00000_01000
+args=$3    # "--resume True --seed_dir <dir>"
+
+data_tr="../gdrive/My Drive/flickr8k_spoken_captions/flickr8k_train.json"
+data_dt="../gdrive/My Drive/flickr8k_spoken_captions/flickr8k_dev.json"
+vqonarg=$(echo $vqon | sed 's/./&,/g' | sed 's/,$//g')  # insert ',' in between
+
+expdir="./exps/$name"
+echo "DIRECTORY: $expdir"
+[ -d "$expdir" ] || mkdir -p "$expdir"
+
+python run_ResDavenetVQ.py --mode train \
+    --VQ-turnon $vqonarg --exp-dir $expdir \
+    --data-train "$data_tr" --data-val "$data_dt" \
+    $args >> "$expdir/train.out" 2>&1
